6f515b79bf8646fe4869c6ece7157c4890b1a247
NullVoxPopuli <199018+NullVoxPopuli@users.noreply.github.com>
2026-05-02 10:39:28 -0400

    Add size-benchmark for hello-world

---
+++ smoke-tests/v2-app-hello-world-template/babel.config.mjs
@@ -0,0 +1,36 @@
+import { buildMacros } from '@embroider/macros/babel';
+
+const macros = buildMacros({});
+
+export default {
+  plugins: [
+    [
+      'babel-plugin-ember-template-compilation',
+      {
+        compilerPath: 'ember-source/dist/ember-template-compiler.js',
+        transforms: [...macros.templateMacros],
+      },
+    ],
+    [
+      'module:decorator-transforms',
+      {
+        runtime: {
+          import: require.resolve('decorator-transforms/runtime-esm'),
+        },
+      },
+    ],
+    [
+      '@babel/plugin-transform-runtime',
+      {
+        absoluteRuntime: __dirname,
+        useESModules: true,
+        regenerator: false,
+      },
+    ],
+    ...macros.babelMacros,
+  ],
+
+  generatorOpts: {
+    compact: false,
+  },
+};
